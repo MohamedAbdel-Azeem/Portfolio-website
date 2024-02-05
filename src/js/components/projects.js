3e4841f95d567b84c5e6f903a8342e4ec54a47e6
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-02-05 19:59:37 +0200

    Projects Page Initialized with Major Bugs

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -1,8 +1,87 @@
+import { projects } from '../data.js';
+import {createDownButton} from "./down-button.js";
+
 export function createProjects(){
-    const projects = document.createElement('section');
-    projects.id = 'projects';
+    const sectionContainer = document.createElement('section');
+    sectionContainer.id = 'projects';
+    sectionContainer.classList.add(
+        "flex",
+        "flex-col",
+        "items-center",
+        "justify-center",
+        "w-full",
+        "space-y-28",
+        "py-20",
+        "px-10",
+        "text-slate-200",
+    );
 
-    
 
-    return projects;
+    const heading = document.createElement('h2');
+    heading.classList.add('text-5xl', 'font-bold', 'text-center');
+    heading.textContent = 'Projects';
+
+
+    const projectsContainer = document.createElement('div');
+    projectsContainer.classList.add( 
+        "projects-container" /*In the Styles.css*/ 
+    );
+
+    projects.forEach((projectElement) => {
+        projectsContainer.appendChild(renderProject(projectElement));
+    });
+
+    sectionContainer.appendChild(heading);
+    sectionContainer.appendChild(projectsContainer);
+    sectionContainer.appendChild(createDownButton('home'));
+    return sectionContainer;
+}
+
+
+function renderProject(project){
+    const ProjectItem = document.createElement('div');
+    ProjectItem.classList.add("project-card");
+
+    const projectContainer = document.createElement('div');
+    projectContainer.classList.add('flex', 'flex-col',"mt-2", 'justify-center',"items-center", 'space-y-2');
+
+    const projectName = document.createElement('h3');
+    projectName.classList.add('text-2xl', 'font-bold');
+    projectName.textContent = project.name;
+
+    const projectImage = document.createElement('img');
+    projectImage.src = project.imageUrl;
+    projectImage.alt = project.name;
+    projectImage.classList.add('w-72', 'h-auto');
+
+    const projectDescription = document.createElement('p');
+    projectDescription.classList.add('text-center',"text-lg");
+    projectDescription.textContent = project.description;
+
+    const projectLanguages = document.createElement('span');
+    projectLanguages.textContent = project.languages;
+
+    const projectLinks = document.createElement('div');
+    projectLinks.classList.add('flex', 'flex-row', 'space-x-4');
+    const repoLink = document.createElement('a');
+    repoLink.href = project.repo;
+    repoLink.target = '_blank';
+    repoLink.textContent = 'Repo';
+    const demoLink = document.createElement('a');
+    demoLink.href = project.demo;
+    demoLink.target = '_blank';
+    demoLink.textContent = 'Demo';
+
+    projectLinks.appendChild(repoLink);
+    projectLinks.appendChild(demoLink);
+
+    projectContainer.appendChild(projectName);
+    projectContainer.appendChild(projectDescription);
+    projectContainer.appendChild(projectLanguages);
+    projectContainer.appendChild(projectLinks);
+
+    ProjectItem.appendChild(projectImage);
+    ProjectItem.appendChild(projectContainer);
+
+    return ProjectItem;
 }
--- src/js/data.js
+++ src/js/data.js
@@ -37,4 +37,37 @@ export const skills = [
     },
 ];
 
-export const projects = [];
+export const projects = [
+    {
+        name: 'Get it Done !',
+        imageUrl: require('../assets/images/projects/project1.png').default,
+        description: 'A Simple to-do list app !',
+        languages: "TailwindCSS . CSS . Javascript",
+        repo: 'https://github.com',
+        demo: 'https://youtube.com'
+    },
+    {
+        name: 'The Last of Us Java',
+        imageUrl: require('../assets/images/projects/project1.png').default,
+        description: 'A Simple App Done in the OOP Course !',
+        languages: "Java . JavaFX . CSS",
+        repo: 'https://github.com',
+        demo: 'https://youtube.com'
+    },
+    {
+        name: 'Student Admin Portal',
+        imageUrl: require('../assets/images/projects/project1.png').default,
+        description: 'A simple portal for the DB Course !',
+        languages: "MS SQL . ASP .NET . Javascript & CSS",
+        repo: 'https://github.com',
+        demo: 'https://youtube.com'
+    },
+    {
+        name: 'Tic-Tac-Toe Game',
+        imageUrl: require('../assets/images/projects/project1.png').default,
+        description: 'A simple Tic Tac Toe Game',
+        languages: "HTML . CSS . Javascript",
+        repo: 'https://github.com',
+        demo: 'https://youtube.com'
+    }
+];
